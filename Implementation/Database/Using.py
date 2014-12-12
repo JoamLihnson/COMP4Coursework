cd420c40bee17ed70c5b371cc0886b79f5a30b3a
Liam Johnson <31715@longroad.ac.uk>
2014-12-12 14:02:42 +0000

    FUCK LUCY

--- Implementation/Database/Using.py
+++ Implementation/Database/Using.py
@@ -1,11 +1,21 @@
 import sqlite3
 
-
-def Query (sql, data):
+#For whatever reason, Adam did it this way, so I am too :v
+#The multiBinds variable is there to prevent binding errors, as if you refernce an unequal amount of fields
+#there is an error, and different functions require different amounts. Insert needs many, delete needs 1
+#So, this method allows both versions of code to run as long as I tell it if its working with more than 1 field
+def Query (sql, data, multiBinds, method):
     with sqlite3.connect("charityShop.db") as db:
         cursor = db.cursor()
         cursor.execute("PRAGMA foreign_keys = ON")
-        cursor.execute(sql, data)
+        if not multiBinds:
+            cursor.execute(sql, (data,))
+        else:
+            cursor.execute(sql, data)
+        if method == "Select":
+            selected = cursor.fetchone()
+            db.commit()
+            return(selected)
         db.commit()
 
         
@@ -58,9 +68,9 @@ def Insert():
         if Questioned == 0:
             return(0)
         cursor = db.cursor()
-        #Since the format of the string is very specfic, it would require it to be hardcoded from every seperate entity and every action
-        #Instead, I have made it so that it manipulates the contents of the ListFields list (which contains the names for every part of every entity)
-        #The string is therefore constucted by using FOR loops and string editing, so this process can be applied to whatever I throw at it
+# Since the format of the string is very specfic, it would require it to be hardcoded from every seperate entity and every action
+# Instead, I have made it so that it manipulates the contents of the ListFields list (which contains the names for every part of every entity)
+# The string is therefore constucted by using FOR loops and string editing, so this process can be applied to whatever I throw at it
         completeList = (ListedFields[0] + " (" + ListedFields[1])
         for count in range(len(ListedFields)-2):
             completeList = (completeList + ", " + ListedFields[count+2])    
@@ -70,13 +80,11 @@ def Insert():
         for count in range(len(ListedFields) - 1):
             values.append(0)
         for count in range(len(ListedFields) - 1):
-            values[count] = input("{} :".format(ListedFields[count +1]))
+            values[count] = input("Enter the {} :".format(ListedFields[count +1]))
         values = tuple(values)
+        Query(sql, values, True, "Insert")
         completion = 1
         return(1)
-
-def Select(Choice):
-    pass
 
 def Delete():
     with sqlite3.connect("charityShop.db") as db:
@@ -90,6 +98,11 @@ def Delete():
         if Choice == 1:
                     print("The code to delete everything does exist, but I'm not letting it run out of danger of it DELETING ALL DATA AJHSJKHAKS")
                     deleteAll = False
+                    print()
+                    print("Would you really like to run it?")
+                    Choice = input(">>")
+                    if Choice in ("Y","y","Yes","yes"):
+                        deleteAll = True
                     if deleteAll:
                         sql = ("delete from Staff")
                         cursor.execute(sql)
@@ -114,12 +127,44 @@ def Delete():
             refData = input(">>")
             print()
             sql = "delete from {} where {}=?".format(ListedFields[0], ListedFields[Choice])
-            Query(sql, refData)
+            print(sql)
+            Query(sql, refData, True, "Delete")
             print("Deleted!")
             print()
         return
 
 def Update():
+    with sqlite3.connect("charityShop.db") as db:
+        cursor = db.cursor()
+        Questioned, ListedFields = EntSelect()
+        print("Enter the primary key for the entry you would like to update: ")
+        primKey = int(input(">>"))
+        print()
+        completeList = (ListedFields[1] + "," + ListedFields[2])
+        for count in range(len(ListedFields)-3):
+            completeList = (completeList + "," + ListedFields[count+3])
+        sql = "select {} from {} where {}=?".format(completeList,ListedFields[0],ListedFields[1])
+        selected = Query(sql, primKey, False, "Select")
+        for count in range(len(ListedFields)-1):
+            print("{}: {}".format(ListedFields[count+1], selected[count]))
+        print()
+        stopRepeat = False
+        while not stopRepeat:
+            print("Which value would you like to change?")
+            for count in range (len(ListedFields)-1):
+                print("{}. {}".format(count+1,ListedFields[count+1]))
+            print("{}. End updating".format(len(ListedFields)))
+            print()
+            Choice = int(input(">>"))
+            if Choice > 0 and Choice < (len(ListedFields)-1):
+                print("Enter a new value for the {} field".format(ListedFields[Choice]))
+                newData = input(">>")
+                print()
+                sql = "update {} set {}=? where {}={}".format(ListedFields[0], ListedFields[Choice], ListedFields[1], primKey)
+                Query(sql, newData, False, "Update")
+                print("Update Successful!")
+            else:
+                stopRepeat = True                     
     return
 
 
@@ -127,6 +172,7 @@ def Update():
 
 
 # All these SelectEntity functions have the atributes of each entity hardcoded in so that they can be manipulated in a way that Python understands
+# This allows for flexability when making changes to different entities, as it won't require indivdual method functions be written for each individual entity
 def SelectStaff():
     Questioned = "(?,?,?)"
     ListedFields = ["Staff", "StaffID", "StaffFirstName", "StaffLastName"]
@@ -167,6 +213,7 @@ if __name__ == "__main__":
                 print("Values added!")
             else:
                 print("Addition aborted")
+            print()
         elif menuSelect == 2:
             Update()
         elif menuSelect == 3:
