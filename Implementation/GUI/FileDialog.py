ead1ad3242753d157b5aa43aaf0bb3609075f3da
Liam Johnson <31715@longroad.ac.uk>
2015-01-07 15:18:32 +0000

    Added File Dialog to main window (ACTUALLY LEL)

--- Implementation/GUI/FileDialog.py
+++ Implementation/GUI/FileDialog.py
@@ -1,1 +1,10 @@
-dvfdsvdsvdsv
+#Code for opening and using the file dialog
+
+from PyQt4.QtGui import *
+
+class FileDialog(QWidget):
+    """Opens a file dialog for file selection"""
+    def __init__(self,windowTitle,fileTypes):
+        super().__init__()
+
+        self.
