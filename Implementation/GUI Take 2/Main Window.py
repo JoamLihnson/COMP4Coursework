fc027ecdd37648b6b5e0463dcaf53f2d961fa30f
Liam Johnson <31715@longroad.ac.uk>
2015-01-16 12:17:22 +0000

    Remember to update your miktek you dumb shit

--- Implementation/GUI Take 2/Main Window.py
+++ Implementation/GUI Take 2/Main Window.py
@@ -9,3 +9,34 @@ class Window (QMainWindow):
         super().__init__()
         
         self.setWindowTitle("Charity Shop Database Management")
+
+        self.grid = QGridLayout()
+        self.setLayout(self.grid)
+        self.button1 = QPushButton("Button 1", self)
+        self.button2 = QPushButton("Button You", self)
+        self.grid.addWidget(self.button1, 0,0,0,0)
+        self.grid.addWidget(self.button2, 1,1,1,1)
+
+        self.menuBar = QMenuBar()
+        self.fileMenu = self.menuBar.addMenu("File")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    Application = QApplication(sys.argv)
+    Window = Window()
+    Window.show()
+    Window.raise_()
+    Application.exec_()
