fd70f47db3ba9626ac2b40bac509c8d039713aa8
Liam Johnson <31715@longroad.ac.uk>
2015-01-22 16:04:59 +0000

    YAY

--- Implementation/GUI Take 2/LETS TRY THIS AGAIN AS YOUR CODE REFUSES TO WORK AAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHH.py
+++ Implementation/GUI Take 2/LETS TRY THIS AGAIN AS YOUR CODE REFUSES TO WORK AAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHH.py
@@ -12,25 +12,63 @@ class Window (QMainWindow):
         self.MakeUI()
 
     def MakeUI(self):
-        self.okButton = QPushButton("OK")
-        self.cancelButton = QPushButton("Cancel")
-        self.textBox = QLineEdit()
-        self.vBox = QVBoxLayout()
-        self.grid = QGridLayout()
-        self.grid.addWidget(self.textBox, 0,0)
-        self.vBox.addWidget(self.okButton)
-        self.vBox.addWidget(self.cancelButton)
-        self.grid.addLayout(self.vBox, 0,1)
-        self.useButton = QPushButton("Useless")
-        self.usesButton = QPushButton("Usessssless")
+        self.Insert = QAction("Insert", self)
+        self.Delete = QAction("Delete", self)
+        self.Update = QAction("Update", self)
+        self.Donate = QAction("Donate", self)
+        self.Search = QAction("Search", self)
+        
 
-        self.grid.addWidget(self.useButton, 1, 1)
-        self.grid.addWidget(self.usesButton, 1, 0)
+
+        self.menuBar = QMenuBar()
+        self.fileMenu = self.menuBar.addMenu("File")
+        self.databaseMenu = self.menuBar.addMenu("Database")
+        self.databaseMenu.addAction(self.Insert)
+        self.databaseMenu.addAction(self.Update)
+        self.databaseMenu.addAction(self.Delete)
+        self.donationMenu = self.menuBar.addMenu("Dontation")
+        self.donationMenu.addAction(self.Donate)
+        self.donationMenu.addAction(self.Search)
+        
+        self.setMenuBar(self.menuBar)
+
+        self.insertButton = QPushButton("Insert")
+        self.updateButton = QPushButton("Update")
+        self.deleteButton = QPushButton("Delete")
+        self.leftVBox = QVBoxLayout()
+        self.leftVBox.addWidget(self.insertButton)
+        self.leftVBox.addWidget(self.updateButton)
+        self.leftVBox.addWidget(self.deleteButton)
+
+        self.donateButton = QPushButton("Donate")
+        self.searchBar = QLineEdit()
+        self.searchGo = QPushButton("Search")
+        self.searchBox  = QHBoxLayout()
+        self.searchBox.addWidget(self.searchBar)
+        self.searchBox.addWidget(self.searchGo)
+        self.rightVBox = QVBoxLayout()
+        self.rightVBox.addWidget(self.donateButton)
+        self.rightVBox.addLayout(self.searchBox)
+
+        self.statusBar = QStatusBar()
+        self.statusBar.setSizeGripEnabled(0)
+
+
+        self.upperHBox = QHBoxLayout()
+        self.upperHBox.addLayout(self.leftVBox)
+        self.upperHBox.addLayout(self.rightVBox)
+
+        self.totalVBox = QVBoxLayout()
+        self.totalVBox.addLayout(self.upperHBox)
+        self.totalVBox.addWidget(self.statusBar)
 
         self.centrelWidget = QWidget()
-        self.centrelWidget.setLayout(self.grid)
+        self.centrelWidget.setLayout(self.totalVBox)
 
         self.setCentralWidget(self.centrelWidget)
+
+    def InsertDialog(self):
+        
 
 
 if __name__ == "__main__":
