c073fc642afcd647258244b52cd5c176718b9a87
Liam Johnson <31715@longroad.ac.uk>
2015-03-13 14:15:18 +0000

    Education

--- Implementation/GUI/Table.py
+++
@@ -1,9 +0,0 @@
-from PyQt.4Gui import *
-
-class TableWidget(QWidget):
-    """Creates a table, given different values"""
-    def __init__(self, rows, columns):
-        super().__init__()
-
-        self.tableWidget = new QTableWidget(12, 3, this);
-        
